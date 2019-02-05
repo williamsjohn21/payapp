bd38a2c655f9338837455b24dddb4ede4c400cea
David Odeyinka <david.odeyinka@yahoo.com>
2019-02-05 13:28:31 +0100

    Set up all the necessary modules

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const request = require('request');
+
+const bodyParser = require('body-parser');
+
+const pug = require('pug');
+
+const _ = require('lodash');
+
+const path = require('path');
+
+const {Donor} = require('./models/donor')
+
+const {initializePayment, verifyPayment} = require('./config/paystack')(request);
