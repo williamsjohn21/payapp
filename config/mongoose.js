c3b1046e92571fe13eec60bd181bf62cbd8092f9
David Odeyinka <david.odeyinka@yahoo.com>
2019-02-05 12:18:17 +0100

    Yet another change

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,2 +1,4 @@
 const mongoose = require ('mongoose');
 mongoose.connect('mongodb://Daveux:D@veuxbap@55w0rdlm67...@ds223015.mlab.com:23015/payments',{useNewUrlParser:true});
+
+module.exports = {mongoose}
