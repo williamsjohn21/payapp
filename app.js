2f34b07eb76b8dfa4b7a32823aea68154280782e
David Odeyinka <david.odeyinka@yahoo.com>
2019-02-05 15:38:52 +0100

    Yet another change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public/')));
 
 app.set('view engine', pug);
