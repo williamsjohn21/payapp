849cd20958eb20c23523e237b8cf8666cb6255cd
David Odeyinka <david.odeyinka@yahoo.com>
2019-02-05 13:01:55 +0100

    Created options object

--- config/paystack.js
+++ config/paystack.js
@@ -2,6 +2,9 @@ const paystack = (request) => {
     const MySecretKey = 'Bearer sk_test_xxxx';
 
     const initializePayment = (form, mycallback) => {
+        const options = {
+
+            url : 'https://api.paystack.co/transaction/initialize',
 
     }
     const verifyPayment = (ref, mycallback) => {
